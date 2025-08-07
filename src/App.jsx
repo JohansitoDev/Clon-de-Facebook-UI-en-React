6b364e59d04407bf9e8da696188dd06dd6bc3183
JohansitoDev <johanmancebo19@gmail.com>
2025-08-07 14:51:04 -0400

    Cambios en el archivo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
 import Login from './components/Login'
 
 function App() {
@@ -9,9 +9,9 @@ function App() {
 
   return (
     <>
- <div className="container">
+
   <Login/>
- </div>
+
     </>
   )
 }
