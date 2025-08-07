cc8e8a6c13924afa2e17f1134f3a6476656be7df
JohansitoDev <johanmancebo19@gmail.com>
2025-08-06 22:24:01 -0400

    JohansitoDev React clon Fcebook UI

---
+++ src/components/Login.jsx
@@ -0,0 +1,22 @@
+function Login(){
+    <div className="container">
+        <div className="card-content">
+            <div className="row">
+                <div className="col-sm-4">
+                    <img src="" alt="esta es una imagen JohansitoDev" />
+                </div>
+                <div className="col-sm-4">
+                    <h4>Login Facebook</h4>
+                    <label htmlFor="">Nombre:</label>
+                    <input type="text" placeholder="Ingresa tu nombre JohansitoDev" />
+                    <label htmlFor="" >Correo Eletronico</label>
+                    <input type="text" placeholder="Ingrese su correo eletronico" />
+                    <label htmlFor="">Password</label>
+                    <input type="password" placeholder="ingrese su contraseña" />
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Login;
