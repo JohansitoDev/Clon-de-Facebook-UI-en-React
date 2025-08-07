7ab6154188780e6d305e44b061bb113ec092a0e3
JohansitoDev <johanmancebo19@gmail.com>
2025-08-07 17:28:08 -0400

    Cambios agregado sintaxis

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-import Login from './components/Login';
-import Navbar from './components/Navbar';
+import {Login }from './components/Login';
+import {Navbar} from './components/Navbar';
 
 function App(){
   return(
